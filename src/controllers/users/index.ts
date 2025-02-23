ca53f6e7c600c3a67e50a2f62e503cb271f8ef74
saber-mekki <saber.mekki6@gmail.com>
2025-02-24 00:02:26 +0100

    feat: add `add user controller` func in `controller user index` file

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -1,6 +1,7 @@
-import { addUser, deleteUser, getUser, getUsers} from "../../services/users";
+import { addUser, deleteUser, login, getUsers} from "../../services/users";
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
+import { jwtTokens } from '../../helpers/index';
 
 export const getUsersController = async (req: Request, res: Response) => {
   const { login, password } = req.body
@@ -13,50 +14,35 @@ export const getUsersController = async (req: Request, res: Response) => {
   }
 };
 
-export const getUserController = async (req: Request, res: Response) => {
-  const { login, password } = req.body
-
-  try {
-    const result = await getUser(login as string, password as string);
-
-    if (result.length === 0) {
-      	res.status(200).send({ error: true, user: [] });
-		}
-
-		const user = result[0];
-		const match = await bcrypt.compare(password, user.password);
-
-		if (match) {
-      let error = result[0] === undefined ? true : false;
-			res.status(200).send({ error: error, user: user });
-        console.log({ user });
-			return user;
-    
-		} else {
-			res.status(200).send({ error: true, user: [] });
-		}
-    
-  } catch (error) {
-  	res.status(200).send({ error: true, user: [] });
-  }
-};
-
 export const addUserController = async (req: Request, res: Response) => {
-  const { login, password, phone, gender, name, registerType } = req.body;
+  const { name, password, email, registerType } = req.body;
   try {
- 
     await addUser(
-			login as string,
-			password as string,
-			phone as number,
-			gender as string,
 			name as string,
+			password as string,
+			email as string,
 			registerType as string,
 		);
     res.status(200).send({ error: false });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: error });
+  }
+};
+
+export const loginController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = await login(email as string, password as string);
+
+    console.log({result})
+    let tokens = jwtTokens(result.user_id, result.user_name, result.user_email);
+    console.log({tokens})
+    res.cookie('refresh_token', tokens.refreshToken, {...(process.env.COOKIE_DOMAIN && {domain: process.env.COOKIE_DOMAIN}) , httpOnly: true,sameSite: 'none', secure: true});
+
+    res.status(200).send({ error: false ,tokens});
+  } catch (Error :any) {
+    return res.status(500).json({ error: Error.message });
   }
 };
 
