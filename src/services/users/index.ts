9497bb2ae9eebb53b8521db3eabacfcb7fffb62d
Fsociety <souhailleaders2003@gmail.com>
2025-02-24 00:27:23 +0100

    fix(services/users): correct `SQL queries` for improved functionality

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,5 +1,8 @@
 import { executeSQLQuery } from "../../database";
+import jwt from 'jsonwebtoken'; 
 import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+
 
 export const getUsers = async (login: string, password: string) => {
     const query = `SELECT * FROM public.users`;
@@ -14,17 +17,58 @@ export const getUser = async (login: string, password: string) => {
     return result.rows;
 };
 
-export const addUser = async (login: string, password: string, phone : number,gender  : string,name  : string,	registerType: string) => {
-      const saltRounds = 10; // You can adjust this value for stronger hashing
-			const hashedPassword = await bcrypt.hash(password, saltRounds);
+export const addUser = async (name: string, email: string, password: string) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-			const query = `INSERT INTO public."userTable"("registerType",login,password,phone,gender,name) VALUES ('${registerType}','${login}','${hashedPassword}','${phone}','${gender}','${name}')`;
-			const result = await executeSQLQuery(query);
-			return result.rows[0];
+  const id = uuidv4();
+
+  const query = `
+    INSERT INTO public."users"(user_id, user_name, user_email, user_password) 
+    VALUES ($1, $2, $3, $4) RETURNING *`;
+  
+  const values = [id, name, email, hashedPassword];
+
+  const result = await executeSQLQuery(query, values);
+  return result.rows[0];
 };
 
-export const deleteUser = async (login: string) => {
-    const query = `DELETE  FROM public.userDB WHERE login='${login}' returning login`;
-    const result = await executeSQLQuery(query);
+export const deleteUser = async (email: string) => {
+    const query = `
+    DELETE FROM public."users" 
+    WHERE user_email = $1
+    RETURNING user_id, user_name, user_email;
+  `;
+    const result = await executeSQLQuery(query,[email]);
+    if(result.rows.length==0){
+        throw new Error(`User with username '${email}' not found.`);
+
+
+    }
     return result.rows[0];
 };
+
+
+export const loginUser = async(email:string,password:string) =>{
+    const query = `SELECT user_email, user_password FROM public."users" WHERE user_email = $1`;
+    const result=await executeSQLQuery(query,[email])
+    if(result.rows[0]==0){
+        throw new Error("Invalid email or password");
+
+    }
+    const user=result.rows[0];
+    
+    const isMatch=await bcrypt.compare(password,user.user_password) 
+    if(!isMatch){
+        throw new Error("Invalid password");
+
+    }
+    const token = jwt.sign(
+        {  email: user.user_email },
+        process.env.JWT_SECRET as string,
+            { expiresIn: "1h" } 
+        );
+     
+      return {  token  };
+
+}
