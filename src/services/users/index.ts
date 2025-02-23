f7098408d4b094d20a89fc5471b602b38619843b
saber-mekki <saber.mekki6@gmail.com>
2025-02-24 00:05:34 +0100

    feat: add `login` func in `services user index` file

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -7,20 +7,33 @@ export const getUsers = async (login: string, password: string) => {
     return result.rows;
 };
 
-export const getUser = async (login: string, password: string) => {
-    const query = `SELECT * FROM public."userTable" WHERE login='${login}'`;
-    
-    const result = await executeSQLQuery(query);
-    return result.rows;
+export const addUser = async (name: string, password: string, email : string,	registerType: string) => {
+      const saltRounds = 10; 
+	  const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const checkQuery = `SELECT user_email FROM public.users WHERE user_email = '${email}'`;
+      const existingUser = await executeSQLQuery(checkQuery);
+      if (existingUser.rows.length > 0) {
+        throw new Error("Email already exists. Please use a different email.");
+      }
+      
+	  const query = `INSERT INTO public.users (user_name,user_email,user_password) VALUES ('${name}','${email}','${hashedPassword}')`;
+      const result = await executeSQLQuery(query);
+	  return result.rows[0];
 };
 
-export const addUser = async (login: string, password: string, phone : number,gender  : string,name  : string,	registerType: string) => {
-      const saltRounds = 10; // You can adjust this value for stronger hashing
-			const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-			const query = `INSERT INTO public."userTable"("registerType",login,password,phone,gender,name) VALUES ('${registerType}','${login}','${hashedPassword}','${phone}','${gender}','${name}')`;
-			const result = await executeSQLQuery(query);
-			return result.rows[0];
+export const login = async (email: string, password: string) => {
+    const users = `SELECT * FROM users WHERE user_email= '${email}'`;
+      const existingUsers = await executeSQLQuery(users);
+      if (existingUsers.rows.length === 0) throw new Error("Email is incorrect");
+       //PASSWORD CHECK
+       const validPassword = await bcrypt.compare(password, existingUsers.rows[0].user_password);
+     console.log({validPassword})
+       if (!validPassword) throw new Error("Incorrect password");
+        //JWT
+        return existingUsers.rows[0]
+     
+
 };
 
 export const deleteUser = async (login: string) => {
