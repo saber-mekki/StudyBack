cf5f52d1274979b3fe6ea79975e1f191873986ef
Fsociety <souhailleaders2003@gmail.com>
2025-03-08 03:54:37 +0100

    fix(database) add `gender` and `phone number`

--- src/database/database.sql
+++ src/database/database.sql
@@ -9,13 +9,19 @@ CREATE TABLE users(
   user_email TEXT NOT NULL UNIQUE,
   user_password TEXT NOT NULL,
   type_register TEXT CHECK (type_register IN ('student', 'tutor','admin')) NOT NULL
+  phone_number TEXT,  
+  gender TEXT CHECK (gender IN ('male', 'female')) 
 );
+
 
 SELECT * FROM users;
 
 INSERT INTO users (user_name,user_email,user_password) VALUES ('test','test@test.com','test');
 
+
+--to test
 INSERT INTO users (user_name,user_email,user_password,type_register) VALUES ('admin','admin@admin.com','$2b$10$C4nknEgHTiJ3igaHDLSmaufW4VujYJl90xxjCY1.Vz.9S3vl7KHei','admin');
+
 --$2b$10$C4nknEgHTiJ3igaHDLSmaufW4VujYJl90xxjCY1.Vz.9S3vl7KHei === '0000'
 
 --
